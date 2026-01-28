ba350043d5c27ff7c1e63cdcb0bf8819ed1efc3f
Adrian <adri.alcolea@gmail.com>
2026-01-28 22:27:25 +0100

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,1 +1,16 @@
+export default async function handler(req, res) {
+  const STATION_ID = "TU_STATION_ID";
+  const API_KEY = "TU_API_KEY";
+
+  const url = `https://api.weathercloud.net/v2/weather/current/${STATION_ID}?key=${API_KEY}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Error obteniendo datos" });
+  }
+}
 
